d2a1d23462717b1ec69c97692f130915607220c7
Dana Rocha <dana.rocha@bigbank.ee>
2019-11-11 17:55:11 +0200

    chore: make folder organization better

---
+++ backend/__tests__/factories.js
@@ -0,0 +1,12 @@
+import faker from 'faker';
+import { factory } from 'factory-girl';
+
+import User from '../src/app/models/User';
+
+factory.define('User', User, {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+});
+
+export default factory;
