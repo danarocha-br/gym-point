1f1b6bc132c2bff9b7486d71bce895d35709cab2
Dana Rocha <dana.rocha@bigbank.ee>
2019-11-11 16:56:17 +0200

    feat: initial test config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,13 @@
-require('dotenv/config');
+require('../bootstrap');
 
 module.exports = {
-  dialect: 'postgres',
+  dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST,
   port: '5433',
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  storage: './__tests__/database.sqlite',
   define: {
     timestamps: true,
     underscored: true,
