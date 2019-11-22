c7d4c2496bee9b809583d30a2681865dc7e8ec38
Dana Rocha <dana.rocha@bigbank.ee>
2019-11-22 21:02:16 +0200

    feat: make checkin component

---
+++ mobile/src/components/DataDisplay/Checkin/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Title, Time } from './styles';
+
+export default function Checkin() {
+  return (
+    <Container>
+      <Title>Test</Title>
+      <Time>Hoje às 14h</Time>
+    </Container>
+  );
+}
