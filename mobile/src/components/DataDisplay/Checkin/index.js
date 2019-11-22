06682eb34fcf6f3585ac11833e535a95fa08a110
Dana Rocha <dana.rocha@bigbank.ee>
2019-11-22 21:34:56 +0200

    feat: get checkins from api

--- mobile/src/components/DataDisplay/Checkin/index.js
+++ mobile/src/components/DataDisplay/Checkin/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { parseISO, formatRelative } from 'date-fns';
 
 import { Container, Title, Time } from './styles';
 
-export default function Checkin() {
+export default function Checkin({ data }) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.createdAt), new Date(), {
+      addSuffix: true,
+    });
+  }, [data.createdAt]);
+
   return (
     <Container>
-      <Title>Test</Title>
-      <Time>Hoje às 14h</Time>
+      <Title>Check-in # 1</Title>
+      <Time>{dateParsed}</Time>
     </Container>
   );
 }
+
+Checkin.propTypes = {
+  /**
+   * Defines the data for the component.
+   */
+  data: PropTypes.object.isRequired,
+};
--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import api from '~/services/api';
 
 import { Container, CheckinList } from './styles';
 import Wrapper from '~/components/Wrapper';
 import Checkin from '~/components/DataDisplay/Checkin';
+import { useSelector } from 'react-redux';
 
-const data = [1, 2, 3, 4, 5];
+const Dashboard = () => {
+  const [checkins, setCheckins] = useState([]);
+  const studentId = useSelector(state => state.student.student.id);
 
-const Dashboard = () => (
-  <Wrapper color="light">
-    <Container>
-      <CheckinList
-        data={data}
-        keyExtractor={item => String(item)}
-        renderItem={({ item }) => <Checkin data={item} />}
-      />
-    </Container>
-  </Wrapper>
-);
+  useEffect(() => {
+    async function loadCheckins() {
+      const response = await api.get(`students/${studentId}/checkins`);
+      setCheckins(response.data);
+    }
+    loadCheckins();
+  }, []);
+
+  return (
+    <Wrapper color="light">
+      <Container>
+        <CheckinList
+          data={checkins}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => <Checkin data={item} />}
+        />
+      </Container>
+    </Wrapper>
+  );
+};
 
 export default Dashboard;
 
