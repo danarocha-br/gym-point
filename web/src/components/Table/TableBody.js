72b04a1be438ffaced818fbae7cc0b0f94023229
Dana Rocha <dana.rocha@bigbank.ee>
2019-12-03 16:29:31 +0200

    [web] feat: make table animated

--- web/src/components/Table/TableBody.js
+++ web/src/components/Table/TableBody.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import posed from 'react-pose';
+
+const Body = posed.tbody({
+  start: {
+    y: '0%',
+    delayChildren: 200,
+    staggerChildren: 50,
+  },
+  end: { y: '-100%', delay: 300 },
+});
+
+const Row = posed.tr({
+  start: { y: 0, opacity: 1 },
+  end: { y: 20, opacity: 0 },
+});
 
 export default function TableBody({ data, columns }) {
+  const [isAnimated, setAnimation] = useState(false);
+
+  useEffect(() => {
+    setTimeout(setAnimation({ isAnimated: !isAnimated }), 1000);
+  }, [isAnimated]);
+
   function createKey(item, column) {
     return item.id + (column.path || column.key);
   }
@@ -14,18 +35,18 @@ export default function TableBody({ data, columns }) {
   }
 
   return (
-    <tbody>
+    <Body pose={isAnimated ? 'start' : 'end'}>
       {data.map(item => (
-        <tr key={item.id}>
+        <Row key={item.id}>
           {columns.map(column => (
             <td key={createKey(item, column)}>
               {renderCell(item, column)}
               {column.suffix && ` ${column.suffix}`}
             </td>
           ))}
-        </tr>
+        </Row>
       ))}
-    </tbody>
+    </Body>
   );
 }
 
